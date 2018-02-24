f4002af2d279923e7edde55993d4915e8f1ffe54
Jorf <dontstutteronmylastname@gmail.com>
2018-02-24 02:43:50 -0800

    Created new function to get active spreadsheet
    TODO: Verify data is pulling

---
+++ script.js
@@ -0,0 +1,11 @@
+//spreadsheetgetter
+//First goal: validate statuses
+//Things to consider: breaking up into sections
+//Possibility of named cell ranges
+//Would have to find out how to have reactive range
+
+
+function getActiveSheet() {
+    var sheet = SpreadsheetApp.getActiveSheet();
+    console.log(sheet);
+}
